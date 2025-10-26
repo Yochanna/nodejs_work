f8a5c1db23bed35362f0341b1d5a664fe50bd011
Jane <razwiedanijane@gmail.com>
2025-10-26 21:05:07 +0200

    Updated Node.js course materials

---
+++ Unit0/Unit0/messages.js
@@ -0,0 +1,7 @@
+let messages = [
+  "You are great!",
+  "You can accomplish anything!",
+  "Success is in your future!"
+];
+
+module.exports.messages = messages;
---
+++ Unit0/Unit0/printMessages.js
@@ -0,0 +1,2 @@
+const messageModule = require('./messages.js');
+messageModule.messages.forEach(m => console.log(m));
